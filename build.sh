62db5f8dafd88cacda82cd777c33148fa0830462
Brayden <Brayden@Kids-iMac.local>
2020-09-25 16:52:15 -0400

    made a build folder

--- build.sh
+++ build.sh
@@ -8,6 +8,9 @@ purgecss --css build/src/tailwind.css --content build/views/index.ejs --output b
 cleancss build/src/tailwind.css -o build/static/styles.min.css
 terser build/static/main.js -o build/static/main.min.js -m -c
 
+sed -i '.bak' 's/styles.css/styles.min.css/g' build/views/index.ejs
+
 rm -rf build/src
 rm build/static/main.js
 rm build/static/styles.css
+rm build/views/index.ejs.bak
